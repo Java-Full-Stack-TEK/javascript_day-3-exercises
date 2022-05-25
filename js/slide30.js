45e46bd62dfb0cc2a0621573985c0315b4e6a305
David Cady <cadykd@gmail.com>
2022-05-25 15:47:38 -0700

    updated guessingGame

--- js/slide30.js
+++ js/slide30.js
@@ -1,17 +1,24 @@
-function GuessingGame() {
-    var input = prompt("Enter a range of numbers (like 30 70):");
-    var range = input.split(" ");
-    var guess = (Math.floor(Math.random() * (Number(range[1]) - Number(range[0])) + Number(range[0])));
-    var stopper = 0;
-    var userGuess = prompt('Guess a number between ${range[0]} and  ${range[1]}');
-    while(stopper <= 10) {
-        if(guess == userGuess){
-            alert("You got it: " + userGuess);
-            break;
-        }
-        userGuess = prompt('Keep guessing a number between ${range[0]} and  ${range[1]}');
-    }
-    if(guess != userGuess){
-        alert("Sorry, the answer was " + guess);
-    }
+function guessingGame() {
+	var input = prompt("Enter a range of numbers (like 30 70):");
+	var range = input.split(" ");
+	var guess = (Math.floor(Math.random() * (Number(range[1]) - Number(range[0])) + Number(range[0])));
+	var stopper = 0;
+	var userGuess = prompt(`Guess a number between ${range[0]} and ${range[1]}`);
+	userGuess = parseInt(userGuess);
+	while(stopper <= 10) {
+		if(guess == userGuess) {
+			alert("You got it: " + userGuess);
+			break;
+		} else if(guess > userGuess) {
+			range[0] = userGuess + 1;
+		} else {
+			range[1] = userGuess - 1;
+		}
+		userGuess = prompt(`Keep guessing a number between ${range[0]} and ${range[1]}`);
+		userGuess = parseInt(userGuess);
+		stopper++;
+	}
+	if(guess != userGuess) {
+		alert("Sorry, the answer was " + guess);
+	}
 }
